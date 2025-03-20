56813053bfb674df138ba54a8416ad3d98abd73c
sonu3563 <sonu@techarchsoftwares.com>
2025-03-20 12:55:53 +0530

    Update apiConfig.js

--- server/config/apiConfig.js
+++ server/config/apiConfig.js
@@ -1,5 +1,5 @@
-const frontend_URL = 'http://localhost:3001';
-const backend_URL = 'http://localhost:3000';
+const frontend_URL = 'https://www.cumulus.rip';
+const backend_URL = 'https://www.cumulus.rip/api';
 module.exports = {
   frontend_URL, backend_URL
 };
