ff52a9f5db817ee6e9a5222131364516fd5e56b3
sonu3563 <sonu@techarchsoftwares.com>
2025-03-28 10:39:17 +0530

    Update Apiconfig.jsx

--- client/src/Component/utils/Apiconfig.jsx
+++ client/src/Component/utils/Apiconfig.jsx
@@ -4,9 +4,9 @@
 // export const API_URL = 'http://16.170.230.178:3000'
 
 
-// export const API_URL = 'https://www.cumulus.rip/api'
-// export const FAPI_URL = 'https://www.cumulus.rip'
+export const API_URL = 'https://www.cumulus.rip/api'
+export const FAPI_URL = 'https://www.cumulus.rip'
 
 
-export const API_URL = 'http://localhost:3000'
-export const FAPI_URL = 'http://localhost:3001'
+// export const API_URL = 'http://localhost:3000'
+// export const FAPI_URL = 'http://localhost:3001'
